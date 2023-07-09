088d1e2790d81ded746abca76c06406775960dda
sarthaksharma058439 <shrmsarthak2662@gmail.com>
2023-07-08 18:13:28 -0700

    statfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'myapp/staticfiles')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
 # Default primary key field type
